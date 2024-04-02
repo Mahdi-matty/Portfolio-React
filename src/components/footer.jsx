b8f49a622f415eb9c79782454603bf09a0413005
mahdi <mmiq69@gmail.com>
2024-04-02 13:40:48 -0400

    'vueportfolio:

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { FaGithub } from 'react-icons/fa';
+import { IoLogoVue } from "react-icons/io5"
 const Footer = () => {
   return (
     <footer>
       <div className='githubRes'>
           <a href='https://github.com/Mahdi-matty'><FaGithub /></a>
+          <a href='https://mahdivue.netlify.app'><IoLogoVue /></a>
         </div>
     </footer>
   );
